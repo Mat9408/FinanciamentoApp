5612f8752f21fd4dabdd002dcedcf3f21f100c72
Matheus Alves <99809494+Mat9408@users.noreply.github.com>
2023-08-29 20:17:05 -0300

    Update Financia.cs
    at

--- FinanciamentoApp/Financia.cs
+++ FinanciamentoApp/Financia.cs
@@ -1,17 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinanciamentoApp
+namespace Conhecimento_Basico
 {
     public class Financia
     {
         public static double CalculoFinanciamento(double valor_emprestimo, double tx_juros, double parcelas)
         {
             double taxa = (valor_emprestimo / 100) * tx_juros;
-            return (valor_emprestimo / parcelas) + taxa;
+            double resultado = (valor_emprestimo / parcelas) + taxa;
+            return resultado;
         }
     }
 
