106accf88abc8b62c93b25c935af7973391d3ea0
Mat9408 <matheus94.ma@gmail.com>
2023-08-29 11:41:00 -0300

    Adicionar arquivos de projeto.

---
+++ FinanciamentoApp/Financia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanciamentoApp
+{
+    public class Financia
+    {
+        public static double CalculoFinanciamento(double valor_emprestimo, double tx_juros, double parcelas)
+        {
+            return (valor_emprestimo * tx_juros * parcelas) / 100;
+        }
+    }
+
+}
---
+++ FinanciamentoApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using FinanciamentoApp;
+
+Console.WriteLine("Aplicação de Financiamento");
+
+double valor_emprestimo = 1000;
+double prestacoes = 10;
+double tx_juros = 5;
+double resultado;
+
+
+resultado = Financia.CalculoFinanciamento(valor_emprestimo, prestacoes, tx_juros);
+Console.WriteLine(resultado);
+
+
+
+
+
+
+
